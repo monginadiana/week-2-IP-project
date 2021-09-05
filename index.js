4b12366a78362468cde00188ef98714bf0da2b22
monginadiana <diana.ondieki@student.moringa.com>
2021-09-06 00:02:49 +0300

    addition of the javascript functions and styling of the submit button

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
 var dob = '';
 var gender = '';
+var submitInput = document.getElementById('submit');
+        var userNameInput = document.getElementById('user-name');
+function isDobValid(dob) {
+    return true;
+        }
+function start() {
+        var dobInput = document.getElementById('dob');
+        var genderInput = document.getElementById('gender');
+        var whenUserEntersDob = () => {
+            var value = dobInput.value;
+            if (isDobValid(value)) {
+                dob = value;
+            }
+            checkAllTheValues();
+        }
+        var whenUserEntersGender = () => {
+            var value = genderInput.value;
+            gender = value;
+            checkAllTheValues();
+        }
+        
+        dobInput.addEventListener('input', whenUserEntersDob)
+        genderInput.addEventListener('input', whenUserEntersGender)
+}
+function checkAllTheValues() {
+    if (name.length > 2 && dob && gender) {
+        submitInput.disabled = false;
+    }
+}
+function getDayOfTheWeekFromDob(dob) {
+    var date = new Date(dob)
+    return date.getDay();
+}
+function getName(dayOfWeek, gender) {
+    if (gender == 'female') {
+        var girlNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+        var name = girlNames[dayOfWeek]
+        return name;
+    }
+
+
+    var boyNames = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+    var name = boyNames[dayOfWeek]
+    return name;
+}
+function whenSubmitButtonIsClicked() {
+    var dayOfWeek = getDayOfTheWeekFromDob(dob)
+    var name = getName(dayOfWeek, gender)
+    userNameInput.innerText = name;
+    console.log({ name, dayOfWeek })
+}
+
+submitInput.addEventListener('click', whenSubmitButtonIsClicked)
+start();
